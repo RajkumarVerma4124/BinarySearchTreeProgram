d5299999ebefdce62a36e0b2a2eac3a51ea28bf0
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-04 14:35:55 +0530

    [Raj] Added:- The binary search tree code and display values

--- BinarySearchTree/Program.cs
+++ BinarySearchTree/Program.cs
@@ -10,7 +10,15 @@ namespace BinarySearchTree
     {
         public static void Main(string[] args)
         {
+            //Displaying the message
             Console.WriteLine("Welcome to the binary search tree program");
+            Console.ReadLine();
+
+            //Creating binary search tree to insert and display node values(UC1)
+            BinSearchTree<int> binaryTree = new BinSearchTree<int>(56);
+            binaryTree.Insert(30);
+            binaryTree.Insert(70);
+            binaryTree.Display();
             Console.ReadLine();
         }
     }
