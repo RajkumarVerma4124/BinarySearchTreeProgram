77a85e36df7ded0b281842cf29283a878e24fbd4
[RajkumarVerma4124] <[vermar4124@gmail.com]>
2022-02-04 15:12:46 +0530

    [Raj] Added:- The binary search tree get size method

--- BinarySearchTree/BinSearchTree.cs
+++ BinarySearchTree/BinSearchTree.cs
@@ -9,6 +9,8 @@ namespace BinarySearchTree
     //Creating binary search tree using generics and IComparable to use CompareTo method in program(UC1)
     public class BinSearchTree<T> where T : IComparable<T>
     {
+        public int leftCount, rightCount;
+
         public T NodeData { get; set; }
         public BinSearchTree<T> LeftTree { get; set; }
         public BinSearchTree<T> RightTree { get; set; }
@@ -20,8 +22,6 @@ namespace BinarySearchTree
             this.RightTree = null;
         }
 
-        int leftCount = 0, rightCount = 0;
-
         //Method to check node and add node value to either left or right by comparing(UC1)
         public void Insert(T data)
         {
@@ -29,16 +29,28 @@ namespace BinarySearchTree
             if ((data.CompareTo(currentNodeValue)) < 0)
             {
                 if (this.LeftTree == null)
+                {
                     this.LeftTree = new BinSearchTree<T>(data);
+                    leftCount++;
+                }
                 else
+                {
+                    leftCount++;
                     this.LeftTree.Insert(data);
+                }
             }
             else
             {
                 if (this.RightTree == null)
+                {
+                    leftCount++;
                     this.RightTree = new BinSearchTree<T>(data);
+                }
                 else
+                {
+                    rightCount++;
                     this.RightTree.Insert(data);
+                }
             }
         }
 
@@ -47,15 +59,19 @@ namespace BinarySearchTree
         {
             if (this.LeftTree != null)
             {
-                leftCount++;
                 LeftTree.Display();
             }
-            Console.Write("Binary Tree  : {0}",NodeData.ToString()+" ");
+            Console.Write(NodeData.ToString()+" ");
             if (this.RightTree != null)
             {
-                rightCount++;
                 RightTree.Display();
             }
+        }
+
+        //Method to get the size of the binary tree(UC2)
+        public void GetSize()
+        {
+            Console.WriteLine("Size of the binary tree is : " + (1 + this.leftCount + this.rightCount)+"\n");
         }
     }
 }
